d0604224c78eb523968028adb311ddc96b9bdebf
GabsBaquie <117845004+GabsBaquie@users.noreply.github.com>
2024-07-26 17:31:52 +0200

    chore: Update page layout to fix overflow issue

--- components/header.tsx
+++ components/header.tsx
@@ -30,9 +30,19 @@ export default function Component() {
             </div>
           </div>
         </CardHeader>
-        <CardFooter className="flex flex-row justify-center gap-2">
-          <GitHubIcon />
-          <Link href="https://github.com/GabsBaquie">GitHub</Link>
+        <CardFooter className="flex flex-row justify-center gap-4">
+          <Link
+            href="https://github.com/GabsBaquie"
+            className="flex gap-2 items-center hover:scale-105 ">
+            <GitHubIcon />
+            GitHub
+          </Link>
+          <Link
+            href="https://www.linkedin.com/in/gabriellebaquie"
+            className="flex gap-2 items-center hover:scale-105 ">
+            <LinkedinIcon />
+            LinkedIn
+          </Link>
         </CardFooter>
       </Card>
 
@@ -263,12 +273,32 @@ function GitHubIcon(props: JSX.IntrinsicAttributes & SVGProps<SVGSVGElement>) {
       width="24"
       height="24"
       viewBox="0 0 24 24"
-      stroke="rgb(188, 192, 199)"
-      fill="rgb(188, 192, 199)">
+      stroke="#fff"
+      fill="#fff">
       <path
         fillRule="evenodd"
         clipRule="evenodd"
         d="M12.026 2c-5.509 0-9.974 4.465-9.974 9.974 0 4.406 2.857 8.145 6.821 9.465.499.09.679-.217.679-.481 0-.237-.008-.865-.011-1.696-2.775.602-3.361-1.338-3.361-1.338-.452-1.152-1.107-1.459-1.107-1.459-.905-.619.069-.605.069-.605 1.002.07 1.527 1.028 1.527 1.028.89 1.524 2.336 1.084 2.902.829.091-.645.351-1.085.635-1.334-2.214-.251-4.542-1.107-4.542-4.93 0-1.087.389-1.979 1.024-2.675-.101-.253-.446-1.268.099-2.64 0 0 .837-.269 2.742 1.021a9.582 9.582 0 0 1 2.496-.336 9.554 9.554 0 0 1 2.496.336c1.906-1.291 2.742-1.021 2.742-1.021.545 1.372.203 2.387.099 2.64.64.696 1.024 1.587 1.024 2.675 0 3.833-2.33 4.675-4.552 4.922.355.308.675.916.675 1.846 0 1.334-.012 2.41-.012 2.737 0 .267.178.577.687.479C19.146 20.115 22 16.379 22 11.974 22 6.465 17.535 2 12.026 2z"></path>
+    </svg>
+  );
+}
+
+function LinkedinIcon(
+  props: JSX.IntrinsicAttributes & SVGProps<SVGSVGElement>
+) {
+  return (
+    <svg
+      fill="none"
+      width="22"
+      height="22"
+      viewBox="0 0 32 32"
+      xmlns="http://www.w3.org/2000/svg">
+      <path
+        clip-rule="evenodd"
+        d="m0 16c0-8.83656 7.16344-16 16-16 8.8366 0 16 7.16344 16 16 0 8.8366-7.1634 16-16 16-8.83656 0-16-7.1634-16-16zm8.2-2.7v10.7h3.4v-10.7zm-.2-3.4c0 1.1.8 1.9 1.9 1.9s1.9-.8 1.9-1.9-.8-1.9-1.9-1.9c-1 0-1.9.8-1.9 1.9zm12.6 14.1h3.2v-6.6c0-3.3-2-4.4-3.9-4.4-1.7 0-2.9 1.1-3.2 1.8v-1.5h-3.2v10.7h3.4v-5.7c0-1.5 1-2.3 2-2.3s1.7.5 1.7 2.2z"
+        fill="#fff"
+        fill-rule="evenodd"
+      />
     </svg>
   );
 }
